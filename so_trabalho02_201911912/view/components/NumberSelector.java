07ee0f2eb1bfc6af6c12030b64859c083b669b48
bonfimalan <alanbonfim2000@gmail.com>
2022-08-11 18:17:57 -0300

    NumberSelector refactor

--- so_trabalho02_201911912/view/components/NumberSelector.java
+++ so_trabalho02_201911912/view/components/NumberSelector.java
@@ -1,76 +1,25 @@
 package view.components;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 
 public class NumberSelector extends HBox {
-  private Label numberLabel;
-  private Button up;
-  private Button down;
-  private VBox buttonContainer;
+  private Slider slider;
+  private Label number;
+  private static final Label SECOND_LABEL = new Label("s"); 
 
-  private int minValue;
-  private int maxValue;
   private int value;
 
-  public static final double HEIGHT = 40;
-
   public NumberSelector(int minValue, int maxValue) {
-    this.minValue = minValue;
-    this.maxValue = maxValue;
-    this.value = minValue;
+    slider = new Slider(minValue, maxValue, minValue);
+    number = new Label(String.valueOf(minValue));
+    slider.valueProperty().addListener(
+      (o, oldValue, newValue) -> {
+        value = newValue.intValue();
+        number.setText(String.valueOf(value));
+      });
 
-    numberSelectorDefault();
-  }
-
-  private void numberSelectorDefault() {
-    // initializing view Nodes
-    up = new Button();
-    up.setId("up-button"); // css id
-    down = new Button();
-    down.setId("down-button"); // css id
-    buttonContainer = new VBox(up, down);
-    numberLabel = new Label (String.valueOf(value));
-
-    // define sizes
-    numberLabel.setPrefHeight(HEIGHT);
-    numberLabel.setPrefWidth(50);
-    up.setPrefHeight(HEIGHT/ 2);
-    down.setPrefHeight(HEIGHT/ 2);
-    up.setMinHeight(HEIGHT/ 2);
-    down.setMinHeight(HEIGHT/ 2);
-
-    // adding events to the buttons
-    up.setOnAction(upButtonActionEvent());
-    down.setOnAction(downButtonActionEvent());
-
-    super.getChildren(). addAll(numberLabel, buttonContainer);
-  }
-
-  public int getValue() {
-    return this.value;
-  }
-
-  private EventHandler<ActionEvent> upButtonActionEvent() {
-    return event -> {
-      if (value < maxValue) {
-        value++;
-        numberLabel.setText(String.valueOf(value) + "s");
-      }
-    };
-  }
-
-  private EventHandler<ActionEvent> downButtonActionEvent() {
-    return event -> {
-      if (value > minValue) {
-        value--;
-        numberLabel.setText(String.valueOf(value) + "s");
-      }
-    };
+    super.getChildren().addAll(slider, number, SECOND_LABEL);
   }
 }
